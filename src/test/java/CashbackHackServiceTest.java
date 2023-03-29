99d0c63330144467b4f3914e1f175e5295ac8aa2
anna_stjarna <artemyeva@gmail.com>
2023-03-29 14:13:54 +0300

    Junit4 branch added with all code

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class CashbackHackServiceTest {
+import org.junit.Assert;
+import org.junit.Test;
+import ru.netology.service.CashbackHackService;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldRemain100IfAmount900() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(900);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldRemain350IfAmount650() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 650;
+        int expected = 350;
+        int actual = service.remain(650);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldRemain1000IfAmount0() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 0;
+        int expected = 1000;
+        int actual = service.remain(0);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldRemain0IfAmount1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        Assert.assertEquals(actual, expected);
+    }
+
 }
