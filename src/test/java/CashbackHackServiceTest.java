c25d7e21ad57c79806b5bc8d83cee1131d003286
anna_stjarna <artemyeva@gmail.com>
2023-03-29 14:19:50 +0300

    fixed errors in uploading java classes

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class CashbackHackServiceTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.netology.service.CashbackHackService;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldRemain100IfAmount900() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(900);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldRemain400IfAmount600() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 600;
+        int expected = 400;
+        int actual = service.remain(600);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldRemain1000IfAmount0() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 0;
+        int expected = 1000;
+        int actual = service.remain(0);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldRemain0IfAmount1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        Assert.assertEquals(actual, expected);
+    }
 }
